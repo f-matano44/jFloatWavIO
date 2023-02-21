91572bd63e2d3838a375fc69f96f6d20d8acbf6e
Fumiyoshi MATANO <f_matano44@yahoo.co.jp>
2023-02-21 12:14:30 +0900

    dev

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,11 +1,14 @@
-import jp.f_matano44.jfloatwavio.*;
+import jp.f_matano44.jfloatwavio.WavInput;
 
 public class Test
 {
     public static void main(String[] args)
     {
         WavInput wav = new WavInput("helloworld.wav");
+        double[] x = wav.getX();
 
-        wav.printAudioFormat();
+        
+
+        // wav.printAudioFormat();
     }
 }
